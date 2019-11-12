028561f4d29be368f75334b15cd2550f118efbe9
Noah Jahn <noah.jahn@sparkbusinessworks.com>
2019-11-11 22:15:08 -0500

    Fix parsed arguments, add else if for each exercise

--- main.py
+++ main.py
@@ -8,10 +8,9 @@ from zig_zag_vacuum import *
 
 from directions import *
 
-
 def main():
     parser = argparse.ArgumentParser(
-        prog="main",
+        prog=sys.argv[0],
         description="Implements a performance-measuring environment simulator " +
                     "for vacuum-cleaner world with modular inputs for sensors, " +
                     "actuators, and environment characteristics (size, shape, " +
@@ -23,7 +22,7 @@ def main():
         nargs=1,
         default=3,
         type=int,
-        choices=range(0,100),
+        # choices=range(0,100),
         help="specify the width of the grid. defaults to 3",
     )
 
@@ -32,17 +31,17 @@ def main():
         nargs=1,
         default=3,
         type=int,
-        choices=range(0,100),
+        # choices=range(0,100),
         help="specify the height of the grid. defaults to 3",
     )
 
     parser.add_argument(
         "--exercise",
         # nargs=1,
-        default='2.8',
+        default="2.8",
         type=str,
-        choices=['2.8','2.9','2.10a','2.10b','2.10c','2.11a',
-                    '2.11b','2.11c','2.11d','2.12a','2.12b'],
+        choices=["2.8","2.9","2.10a","2.10b","2.10c","2.11a",
+                    "2.11b","2.11c","2.11d","2.12a","2.12b"],
         help="specify the exercise to run. defaults to 2.8",
     )
 
@@ -51,9 +50,32 @@ def main():
         sys.exit(1)
 
     args = parser.parse_args()
-    envWidth = args.width[0]
-    envHeight = args.height[0]
-    exercise = args.exercise[0]
+    envWidth = args.width
+    envHeight = args.height
+    exercise = args.exercise
+
+    if exercise == '2.8':
+        print('2.8')
+    elif exercise == '2.9':
+        print('2.9')
+    elif exercise == '2.10a':
+        print('2.10a')
+    elif exercise == '2.10b':
+        print('2.10b')
+    elif exercise == '2.10c':
+        print('2.10c')
+    elif exercise == '2.11a':
+        print('2.11a')
+    elif exercise == '2.11b':
+        print('2.11b')
+    elif exercise == '2.11c':
+        print('2.11c')
+    elif exercise == '2.11d':
+        print('2.11d')
+    elif exercise == '2.12a':
+        print('2.12a')
+    elif exercise == '2.12b':
+        print('2.12b')
 
     vacuumWorld = Environment(10,10)
     vacuumWorld.RandomizeWithoutWalls()
