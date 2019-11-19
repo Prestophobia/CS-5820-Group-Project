a9ae8b33cb49d8554836fee1ce704d7a8e1a35c5
noahjahn <36347998+noahjahn@users.noreply.github.com>
2019-11-19 12:54:09 -0500

    Add script to run all exercises

---
+++ run-exercises.ps1
@@ -0,0 +1,22 @@
+python3 main.py --width 5 --height 5 --exercise 2.8
+PAUSE
+python3 main.py --width 5 --height 5 --exercise 2.9
+PAUSE
+python3 main.py --width 5 --height 5 --exercise 2.10a
+PAUSE
+python3 main.py --width 5 --height 5 --exercise 2.10b
+PAUSE
+python3 main.py --width 5 --height 5 --exercise 2.10c
+PAUSE
+python3 main.py --width 5 --height 5 --exercise 2.11a
+PAUSE
+python3 main.py --width 5 --height 5 --exercise 2.11b
+PAUSE
+python3 main.py --width 5 --height 5 --exercise 2.11c
+PAUSE
+python3 main.py --width 5 --height 5 --exercise 2.11d
+PAUSE
+python3 main.py --width 5 --height 5 --exercise 2.12a
+PAUSE
+python3 main.py --width 5 --height 5 --exercise 2.12b
+PAUSE
