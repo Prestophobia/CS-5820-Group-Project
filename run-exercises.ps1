b3aeeba756729fdf02f6d854600e374890e6d261
noahjahn <36347998+noahjahn@users.noreply.github.com>
2019-11-19 12:56:06 -0500

    Change second exercise dimensions

--- run-exercises.ps1
+++ run-exercises.ps1
@@ -1,6 +1,6 @@
 python3 main.py --width 5 --height 5 --exercise 2.8
 PAUSE
-python3 main.py --width 5 --height 5 --exercise 2.9
+python3 main.py --width 3 --height 3 --exercise 2.9
 PAUSE
 python3 main.py --width 5 --height 5 --exercise 2.10a
 PAUSE
